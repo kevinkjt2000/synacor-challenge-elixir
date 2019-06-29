41798ffba09926350c20088e00db41c3704fd907
Kevin Tindall <kevinkjt2000@gmail.com>
2019-06-28 20:31:19 -0500

    Generate mix project
    mix new . --sup --app synacor

---
+++ lib/synacor.ex
@@ -0,0 +1,18 @@
+defmodule Synacor do
+  @moduledoc """
+  Documentation for Synacor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Synacor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/synacor_test.exs
@@ -0,0 +1,8 @@
+defmodule SynacorTest do
+  use ExUnit.Case
+  doctest Synacor
+
+  test "greets the world" do
+    assert Synacor.hello() == :world
+  end
+end
