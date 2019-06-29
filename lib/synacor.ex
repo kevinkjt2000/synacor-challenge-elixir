63239fdcf1e0660bb3f33ab621524ef1e62f2217
Kevin Tindall <kevinkjt2000@gmail.com>
2019-06-28 21:26:34 -0500

    Move pieces into mix project

--- lib/synacor.ex
+++ lib/synacor.ex
@@ -1,18 +1,85 @@
 defmodule Synacor do
-  @moduledoc """
-  Documentation for Synacor.
-  """
+  defp load_program() do
+    filename = "challenge.bin"
+    {:ok, file} = File.open(filename, [:binary, :read])
 
-  @doc """
-  Hello world.
+    file
+    |> IO.binread(:all)
+    |> :binary.bin_to_list()
+  end
 
-  ## Examples
+  def main() do
+    load_program()
+    |> Enum.map(fn
+      0 ->
+        "halt()"
 
-      iex> Synacor.hello()
-      :world
+      1 ->
+        "set(reg_a, b)"
 
-  """
-  def hello do
-    :world
+      2 ->
+        "push(a)"
+
+      3 ->
+        "pop(a)"
+
+      4 ->
+        "eq(a, b, c)"
+
+      5 ->
+        "gt(a, b, c)"
+
+      6 ->
+        "jmp(a)"
+
+      7 ->
+        "jt(a, b)"
+
+      8 ->
+        "jf(a, b)"
+
+      9 ->
+        "add(a, b, c)"
+
+      10 ->
+        "mult(a, b, c)"
+
+      11 ->
+        "mod(a, b, c)"
+
+      12 ->
+        "and(a, b, c)"
+
+      13 ->
+        "or(a, b, c)"
+
+      14 ->
+        "not(a, b)"
+
+      15 ->
+        "rmem(a, mem_b)"
+
+      16 ->
+        "wmem(mem_a, b)"
+
+      17 ->
+        "call(a)"
+
+      18 ->
+        "ret()"
+
+      19 ->
+        "out(a)"
+
+      20 ->
+        "in(a)"
+
+      21 ->
+        "noop"
+
+      _ ->
+        "invalid opcode"
+    end)
+    |> Enum.map(&IO.puts(&1))
   end
 end
